39d23c12a18a2742ace72674b035ddaa9a625fdd
Nurlybek <nurlybek.dev@gmail.com>
2018-10-24 09:31:14 +0600

    Update

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
-# Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields' : ['question_text']}),
+        ('Date information', {'fields' : ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.site_header = 'Polls Administration'
+admin.site.register(Question, QuestionAdmin)
--- numpy-test.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-data1, data2, data3, data4 = np.random.randn(4, 10)
-fig, (ax1, ax2) = plt.subplots(1, 2)
-param = {'marker': 'x'}
-ax1.plot(data1, data2, **param)
-ax2.plot(data3, data4, **param)
-plt.show()
